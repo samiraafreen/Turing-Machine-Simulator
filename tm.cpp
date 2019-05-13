6fc7ba52e7e748d6219cdf01d693317a1204ecf8
safreen@vassar.edu <safreen@sp309-170.cs.vassar.edu>
2019-05-13 00:13:27 -0400

    made changes to tm

--- tm.cpp
+++ tm.cpp
@@ -1,13 +1,27 @@
 #include <stdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
 
 int main(int argc, char* argv[], char* input){
-    
+    // number of states
+    int numStates;
+    // number of final states
+    int[] finalStates;
+    int count;
     // opening the file
     string line;
     ifstream myfile (argv[1]);
     if (myfile.is_open()){
         while (getline(myfile, line)){
-
+            std::string numStates = line[0];
+            numStates = std::stoi(numStates);
+            finalStates = new int[numStates];
+            count++;
+            if (count == 2) {
+                
+            }
         }
 
     }
