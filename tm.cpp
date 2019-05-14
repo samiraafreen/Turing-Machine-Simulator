05b0fc3c90e1a8a3fbae5c9475726f6715ab52fa
safreen@vassar.edu <safreen@newman.cs.vassar.edu>
2019-05-14 11:19:41 -0400

    reading final states

--- tm.cpp
+++ tm.cpp
@@ -15,15 +15,38 @@ int main(int argc, char* argv[], char* input){
     ifstream myfile (argv[1]);
     if (myfile.is_open()){
         while (getline(myfile, line)){
-            std::string numStates = line[0];
-            numStates = std::stoi(numStates);
-            finalStates = new int[numStates];
-            count++;
-            if (count == 2) {
-                
+            int state = 0;
+            // first line = number of states
+            if (count == 0){
+                std::string numStates = line;
+                numStates = std::stoi(numStates);
+                finalStates = new int[numStates];
             }
-        }
+            // second line = alphabet
+            else if (count == 1){
+                char[] alphabet = line;
+            }
+            // third line = list of final states
+            else if (count == 2){
+                char[] charfinalStates = line;
+                for (int i = 0; i < numStates; i++){
+                    if (isdigit(charfinalStates[i])){
+                        // this could be wrong
+                        int finalState = (int)charfinalState[i];
+                        finalStates[i] = finalState;
+                    }
+                } 
+            }
+            // line 4 = transitions start
+            else if (count == 3){
+                char[] transition = line;
+            }
+            count++;
 
+        }
+        myfile.close();
+
+        // read the input
     }
     else cout << "Unable to open file \n" << endl;
 
